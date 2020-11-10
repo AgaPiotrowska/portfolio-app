69ca3a707ac865bd5f8d488bf4b209b217810fd1
AgaPiotrowska <agniegrabo@gmail.com>
2020-11-10 13:18:24 +0100

    added Nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AboutMe from "./pages/AboutMe";
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
         <GlobalStyle/>
-      <AboutMe/>
+        <Nav/>
+        <AboutMe/>
     </div>
   );
 }
