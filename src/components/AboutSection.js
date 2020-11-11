4a5d478c9fa4db5c834a680470b22884ac649e76
AgaPiotrowska <agniegrabo@gmail.com>
2020-11-11 17:39:17 +0100

    animations added - part 2 about section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import aga from "../img/aga.jpg";
 import {About, Description, Image, Hide} from "../styles";
 import {motion} from "framer-motion";
+import {titleAnim, fade, photoAnim} from "../animation";
 
 const AboutSection = () => {
 
@@ -10,28 +11,35 @@ const AboutSection = () => {
             <Description>
                 <motion.div className="title">
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             I work to help ensure
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             your <span>business </span>succeeds
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>and grows.
+                        <motion.h2 variants={titleAnim}>and grows.
                         </motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Contact me if you are looking for self-motivated, well-organized and committed junior front-end with a positive attitude to challenges and opportunities.</p>
-                <button>Contact me</button>
+                <motion.p variants={fade}>
+                    Contact me if you are looking for self-motivated, well-organized and committed junior front-end with a positive attitude to challenges and opportunities.
+                </motion.p>
+                <motion.button
+                    variants={fade}
+                >
+                    Contact me
+                </motion.button>
             </Description>
             <Image>
-                <img src={aga} alt="main picture"/>
+                <motion.img variants={photoAnim} src={aga} alt="main picture"/>
             </Image>
         </About>
     );
 };
 
 export default AboutSection;
+
